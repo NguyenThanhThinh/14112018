66de65ba4d6763f17eafe361c9a03a2797caadda
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-11-14 19:20:29 +0700

    add project

---
+++ 14112018/14112018/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _14112018.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
